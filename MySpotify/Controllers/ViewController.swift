0b458528facb0253c4d4b38e7f0673841244d972
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2016-10-26 09:07:11 +0200

    Fetching code for token

--- MySpotify/Controllers/ViewController.swift
+++ MySpotify/Controllers/ViewController.swift
@@ -12,12 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        requestToken()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func requestToken() {
+        if let requestTokenURL = SPTAuth.loginURL(forClientId: AppConstants.clientID, withRedirectURL: URL(string: AppConstants.redirectURI)!, scopes: [], responseType: "code", allowNativeLogin: true) {
+            UIApplication.shared.openURL(requestTokenURL)
+        }
     }
 
 
