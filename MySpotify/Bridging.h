2ea2ba937563a35d0148fb354ed35e2562bf9c6a
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2016-10-26 08:51:57 +0200

    Setup Cocoapods: Unbox, Alamofire. Spotify SDK added

---
+++ MySpotify/Bridging.h
@@ -0,0 +1,9 @@
+//
+//  Bridging.h
+//  MySpotify
+//
+//  Created by Vladislav Fitc on 26.10.16.
+//  Copyright © 2016 Fitc. All rights reserved.
+//
+
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
