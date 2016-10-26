9786f1b0c8f6818e10966b9402a79ab0474c5aea
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2016-10-26 08:59:29 +0200

    AppConstants added

---
+++ MySpotify/Utils/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  MySpotify
+//
+//  Created by Vladislav Fitc on 26.10.16.
+//  Copyright © 2016 Fitc. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let clientID = "8d8aff2c17f8473e892b5566953d29b9"
+    static let clientSecret = "de50867c925e475c94736e3f318505f8"
+    static let redirectURI = "MySpotify://"
+}
